ff33fa428626b9c04865fa260d0a7d7ff79c93c5
unknown <lmphuthi09@gmail.com>
2020-08-13 11:49:20 +0200

    removing comment functionality added

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -366,6 +366,54 @@ router.patch('/commentpost',async (req,res) => {
     }
 });
 
+//removing comment
+router.patch('/deletecommentpost',async (req,res) => {
+    //check if logged in
+    if(req.session.user){
+
+            if(!isEmpty(req.session.user._id) && !isEmpty(req.body.postId) ){
+                  //adding user comment/id and message to comments database
+                try{
+                    //retrive comment data to compare and see what needs to be updated
+                    const getSpecifiedComment = await Comments.findOne({idOfPost:req.body.postId,_id:req.body._id},(err,docs) => {
+                        if( err || !docs) {
+                            res.json({message:"No data added"});
+                        } else {    
+                           return docs;
+                        };
+                    });
+
+                    if(!isEmpty(getSpecifiedComment)){
+                        //add comment to array 
+                        const saveComments = Comments.updateOne({idOfPost:getSpecifiedComment.idOfPost,_id:getSpecifiedComment._id},
+                            {
+                                $pull :{
+                                    UIMC : {_id: req.body.commentId,
+                                        idOfUserCommenting : req.session.user._id
+                                        }
+                                },
+                            },(err,docs) => {
+                            if( err || !docs) {
+                                res.json({message:"No data add"});
+                            } else {    
+                                res.json({message:"comment removed"});
+                            };
+                        
+                            });
+                          
+                }
+                
+                }catch(err){
+                        
+                    res.json({message:err});
+                }
+            }else{
+                res.json({status:"Comments Not Removed"});
+            }
+    }else{
+        res.json({message:"You Not Logged In"});
+    }
+});
 //functions for create comments schema and likes schema
 
 async function createCommentsSchema(idOfPostVal){
